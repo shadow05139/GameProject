d267498439330c4bcb5ee9c64df4eb3d49f65453
shadow05139 <31062962+shadow05139@users.noreply.github.com>
2017-10-05 15:20:47 +1300

    added 2 levels
    need deathzones coded proberly and and exit zone for levels end and add some badies

---
+++ UnityGameProject/Assets/scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public GameObject activeCheckpoint;
+
+    private PlayerController player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RespawnPlayer()
+    {
+        player.transform.position = activeCheckpoint.transform.position;
+    }
+}
---
+++ UnityGameProject/Assets/scripts/TitleMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour {
+
+	public string firstLevel;
+
+	public void NewGame() {
+		SceneManager.LoadSceneAsync (firstLevel);
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+}

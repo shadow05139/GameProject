d1f9415a45d5f39789bb928c8970029aab548305
shadow05139 <31062962+shadow05139@users.noreply.github.com>
2017-10-21 11:27:25 +1300

    added assets

---
+++ UnityGameProject - workspace/Assets/scripts/EnemyAI1.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (Seeker))]
+public class EnemyAI1 : MonoBehaviour {
+    
+	public Transform target;
+	
+	public float updateRate = 2f;
+	
+	private Seeker seeker;
+	private Rigidbody2D rb;
+	
+	public Path path;
+	
+	public float speed = 300f;
+	public ForceMode2D fMode;
+	
+	[HideInInspector]
+	public bool pathIsEnded = false;
+	
+	public float nextWaypointDistance = 3;
+	
+	private int currentWaypoint = 0;
+	
+	void Start () {
+		seeker = GetComponent<Seeker>();
+		rb = GetComponent<Rigidbody2D>();
+		
+		if (target == null) {
+			Debug.LogError ("no player found");
+			return;
+		}
+		
+		seeker.StartPath (transform.position, target.position, OnPathComplete);
+		
+		StartCoroutine (UpdatePath ());
+	}
+	
+	IEnumerator UpdatePath () {
+		if (target == null) {
+		}
+		
+		seeker.StartPath (transform.position, target.position, OnPathComplete);
+		
+		yield return new WaitForSeconds ( 1f/updateRate );
+		StartCoroutine (UpdatePath());
+	}
+	
+	public void OnPathComplete (Path p) {
+		Debug.Log ("path!");
+		if (!p.error) {
+			path = p;
+			currentWaypoint = 0;
+		}
+	}
+	
+	void FixedUpdate () {
+		if (target == null) {
+			return;
+		}
+		
+		
+		if (path == null)
+			return;
+		
+		if (currentWaypoint >= path.vectorPath.Count) {
+			if (pathIsEnded)
+				return;
+			
+			Debug.Log ("end of path");
+			pathIsEnded = true;
+			return;
+		}
+		pathIsEnded = false;
+	
+		Vector3 dir = ( path.vectorPath[currentWaypoint] - transform.position ).normalized;
+		dir *= speed * Time.fixedDeltaTime;
+		
+		rb.AddForce (dir, fMode);
+		
+		float dist = Vector3.Distance (transform.position, path.vectorPath[currentWaypoint]);
+		if (dist < nextWaypointDistance) {
+			currentWaypoint++;
+			return;
+		}
+	}
+	
+}
---
+++ UnityGameProject - workspace/Assets/scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+	public string titleScene;
+	public string levelSelectScene;
+	public bool isPaused;
+	public GameObject pauseCanvas;
+	void Start() {
+
+	}
+
+	void Update() {
+
+		if(isPaused) {
+			pauseCanvas.SetActive(true);
+			Time.timeScale = 0f;
+		} else {
+			pauseCanvas.SetActive(false);
+			Time.timeScale = 1f;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			isPaused = !isPaused;
+		}
+
+	}
+
+	public void LevelSelect() {
+		SceneManager.LoadSceneAsync (levelSelectScene);
+	}
+
+	public void QuitToMenu() {
+		SceneManager.LoadSceneAsync (titleScene);
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+}
